4fd4ee8c5455474afe3801cd66d11ef7e54c69b5
MBHM9 <mohammadbhm8@gmail.com>
2023-04-14 17:46:18 +0330

    add sample data for admin user and flights for test code

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Flight_schedule[] Flight_schedule = new Flight_schedule[1000];
+
+
+        Flight_schedule[0] = new Flight_schedule("WX-12", "Yazd", "Tehran", 1402, 5, 2, 12, 15, 700, 100,0);
+        Flight_schedule[1] = new Flight_schedule("GA-12", "Yazd", "Esfahan", 1402, 5, 5, 22, 30, 400, 50,0);
+
+        Passenger_data[] Passenger_data = new Passenger_data[10000];
+        Passenger_data[0] = new Passenger_data("mohammad", "1234", 5000, null);
+        Passenger_data[1] = new Passenger_data("Ali", "5863", 1500, null);
+
+        Admin_data[] Admin_data = new Admin_data[10];
+        Admin_data[0] = new Admin_data("admin1", "123456");
+        Admin_data[1] = new Admin_data("admin2", "78910");
 
 
 
